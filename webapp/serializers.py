6026e71c6283a6efd169ab2e329ffa29bfd8a07d
Benson <bensonmakau2000@gmail.com>
2022-02-10 00:18:33 +0300

    build api

--- webapp/serializers.py
+++ webapp/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from rest_framework import employees
+from . models import employees
 
-class employeeSerializer(serializers.ModelSerializer):
+class employeesSerializer(serializers.ModelSerializer):
     class Meta:
         model = employees
         # fields = ('firstname','lastname')
-    fields ='__all__'
+        fields ='__all__'
         
         
         
